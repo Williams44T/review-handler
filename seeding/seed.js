92b31261ad94359a210dd5b5ddfec24a913a987d
Travis Williams <williams44t@gmail.com>
2020-11-13 14:38:09 -0800

    adds  seedReviews fn for seed.js; also adds helper function createImages

--- seeding/seed.js
+++ seeding/seed.js
@@ -1,6 +1,8 @@
 var ratingCategoryData = require('./ratingCategoryData'); //array of objects
 var Products = require('../db/models/products.js');
+var Reviews = require('../db/models/reviews.js');
 var mongoose = require('mongoose');
+var fake = require('faker');
 
 mongoose.connect('mongodb://localhost/adidas');
 
@@ -11,11 +13,11 @@ var shuffle = (arr) => {
   }
 };
 
-var createProducts = (qty) => {
+var seedProducts = (qty) => {
   var products = [];
 
   for (var i = 0; i < qty; i++) {
-    var categoryQty = Math.floor(Math.random() * 4) + 2 //min of 2, max of 5
+    var categoryQty = Math.floor(Math.random() * 4) + 2; //min of 2, max of 5
     shuffle(ratingCategoryData);
     var product = {
       product_id: i,
@@ -28,7 +30,53 @@ var createProducts = (qty) => {
   return products;
 };
 
+var createImages = (maxQty) => {
+  var images = [];
+  var qty = Math.floor(Math.random() * maxQty)//0 is a possibility
+
+  for (var i = 0; i < qty; i++) {
+    images.push(fake.image.cats());
+  }
+
+  return images;
+}
+
+var seedReviews = (products, min, max) => {
+
+  for (var i = 0; i < products.length; i++) {
+    var reviewQty = Math.floor(Math.random() * (max - min)) + min;
+
+    for (var j = 0; j < reviewQty; j++) {
+      var rating = Math.floor(Math.random() * 5) + 1; //min of 1, max of 5
+      var review = {
+        product_id: products[i].product_id,
+        rating,
+        recommended: rating > 2 ? true : false,
+        created: fake.date.between('2017-03-01', new Date()),
+        username: fake.internet.userName(),
+        summary: fake.lorem.sentence(),
+        detail: fake.lorem.paragraph(),
+        verified: [true, false][Math.floor(Math.random() * 2)],
+        photos: createImages(4),
+        helpful: {
+          yes: Math.floor(Math.random() * 10),
+          no: Math.floor(Math.random() * 10)
+        },
+        category_ratings: products[i].rating_categories.map(category => {
+          return {
+            name: category.name,
+            rating: Math.floor(Math.random() * 5) + 1 //min of 1, max of 5
+          };
+        })
+      }
+      Reviews.insertOne(review);
+    }
+  }
+}
+
 Products.removeAll(() => {
-  var products = createProducts(2);
-  console.log(products);
+  var products = seedProducts(2);
+  Reviews.removeAll(() => {
+    seedReviews(products, 1, 5);
+  });
 });
