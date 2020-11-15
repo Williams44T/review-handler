6d4f6ab0f0a52f105b0609f9106a89e6d4032721
Travis Williams <williams44t@gmail.com>
2020-11-15 14:16:10 -0800

    adds findOne method to products model

--- db/models/products.js
+++ db/models/products.js
@@ -25,5 +25,8 @@ var removeAll = (next=()=>{}) => {
   ProductModel.db.dropCollection('products', next);
 };
 
-module.exports.insertOne = insertOne;
-module.exports.removeAll = removeAll;
+var findOne = (id, next=()=>{}) => {
+  ProductModel.find({product_id: id}, next);
+};
+
+module.exports = {insertOne, removeAll, findOne};
