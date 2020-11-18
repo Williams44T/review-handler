4532beeb0ab5be7a6394473e0eb2b21d054c4db1
Travis Williams <williams44t@gmail.com>
2020-11-17 22:18:43 -0800

    installs webpack and webpack-cli; sets webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js'
+  }
+};
