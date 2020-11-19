54844357d04f3ff37079710348caaa2f32f20fe4
Travis Williams <williams44t@gmail.com>
2020-11-18 22:37:30 -0800

    converts App to class component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
-var App = (props) => (<div>
-<div>please let this work</div>
-<div>{props.product.reviews[0].username}</div>
-</div>);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (<div>
+      <div>please let this work</div>
+      <div>{this.props.product.reviews[0].username}</div>
+    </div>);
+  }
+
+}
 
 export default App;
