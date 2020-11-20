ff2f8e2c14fc0ef29701aad4671c7f0730f335a4
Travis Williams <williams44t@gmail.com>
2020-11-20 10:07:09 -0800

    refactors Reviews to be a functional component

--- client/components/Reviews.jsx
+++ client/components/Reviews.jsx
@@ -2,28 +2,14 @@ import React from 'react';
 import Review from './Review.jsx';
 import Actions from './Actions.jsx';
 
-class Reviews extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      reviewCount: 2
-    }
-  }
-
-  loadMoreReviews() { this.setState({reviewCount: this.state.reviewCount + 5}) }
-
-  render() {
-    return (<div>
-      {this.props.reviews.slice(0, this.props.reviewCount).map(review => (
-        <Review
-          review={review}
-          key={review._id}
-        />
-      ))}
-      <Actions onClick={this.props.onClick} />
-    </div>);
-  }
-
-}
+var Reviews = (props) => (<div>
+  {props.reviews.slice(0, props.reviewCount).map(review => (
+    <Review
+      review={review}
+      key={review._id}
+    />
+  ))}
+  <Actions onClick={props.onClick} />
+</div>);
 
 export default Reviews;
