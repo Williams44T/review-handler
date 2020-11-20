66af4055111e71d82eb8e60abd993ae5bd05418a
Travis Williams <williams44t@gmail.com>
2020-11-19 23:44:50 -0800

    converts product.reviews.created strings into actual dates

--- client/index.js
+++ client/index.js
@@ -3,7 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
+//dates (review.created) fresh from the db look like this: 2018-11-27T11:14:53.572Z
+var convertDates = (reviews) => {
+  reviews.forEach(review => {
+    var dateAndTime = review.created.split('.')[0].split('T'); //[2018-11-27, 11:14:53]
+    var parts = dateAndTime[0].split('-').concat(dateAndTime[1].split(':')); //[2018, 11, 27, 11, 14, 53]
+    review.created = new Date(...parts);
+  });
+};
+
 $.get('/products/44/reviews', (product) => {
+  convertDates(product.reviews);
   console.log(product);
   ReactDOM.render(<App product={product} />, document.getElementById('app'));
 });
